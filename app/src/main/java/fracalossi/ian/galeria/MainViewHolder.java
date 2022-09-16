573cd0061080e682dc735a926d690dc36132a9f4
20201TIIMI0080 <ianluiz.fracalossi@gmail.com>
2022-09-16 14:20:55 -0300

    finalizado

---
+++ app/src/main/java/fracalossi/ian/galeria/MainViewHolder.java
@@ -0,0 +1,12 @@
+package fracalossi.ian.galeria;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainViewHolder  extends RecyclerView.ViewHolder {
+    public MainViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
